2fb83827c7a5642b5b37381723755837a03ab33f
JaredPowers <jarpow44@gmail.com>
2016-07-20 17:39:07 -0600

    sorted and got .txt working

--- app/controllers/syndication_controller.rb
+++ app/controllers/syndication_controller.rb
@@ -1,11 +1,13 @@
 class SyndicationController < ApplicationController
 
   def show_tours
-    @tour_urls = Syndication.where(name: 'tour_urls.txt').all
+    @tour_urls = Syndication.where(name: 'tour_urls.txt').all.order('created_at DESC')
+    render formats: :txt
   end
 
   def show_slides
-    @slide_urls = Syndication.where(name: 'tour_slides_descriptions.txt').all
+    @slide_urls = Syndication.where(name: 'tour_slides_descriptions.txt').all.order('created_at DESC')
+    render formats: :txt
   end
-  
+
 end
